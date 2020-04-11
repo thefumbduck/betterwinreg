3c0ba51de1c831f8c549adfa179f519ae2e7d598
thefumbduck <51268203+thefumbduck@users.noreply.github.com>
2020-04-12 00:28:17 +0200

    Add key and value manipulation tests

--- tests/test_key.py
+++ tests/test_key.py
@@ -1,4 +1,5 @@
 from betterwinreg.key import RegistryKey, RegistryPath
+from betterwinreg.value import RegistryValue, RegistryValueType
 
 
 class TestKeyParsing:
@@ -11,3 +12,22 @@ class TestKeyParsing:
     
     def test_computer_prefix(self):
         assert RegistryKey(r'Computer\HKEY_CLASSES_ROOT\Directory\shell').hkey
+
+
+class TestKeyManipulation:
+
+    SET_TEST_KEY_PATH = r'HKEY_CURRENT_USER\harmless_test'
+
+    def test_get(self):
+        assert RegistryKey(r'Computer\HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer')['UserSignedIn'].value == 1
+    
+    def test_set(self):
+        key = RegistryKey(self.SET_TEST_KEY_PATH)
+        key['test'] = RegistryValue(42, RegistryValueType.DWORD)
+        assert key['test'].value == 42
+    
+    def test_del(self):
+        key = RegistryKey(self.SET_TEST_KEY_PATH)
+        key['test'] = RegistryValue(42, RegistryValueType.DWORD)
+        del key['test']
+        assert 'test' not in key
