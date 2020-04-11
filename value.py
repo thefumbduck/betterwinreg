f1f51a51db108c3870e33c90b008110a8cdeb8e2
thefumbduck <loidiegopes@gmail.com>
2020-04-11 18:50:44 +0200

    Remove unrelated values from RegistryValueType

--- value.py
+++ value.py
@@ -2,8 +2,12 @@ from typing import Any, NamedTuple
 from enum import Enum
 import winreg
 
+VALID_VALUE_TYPE_NAMES = [
+    'BINARY', 'DWORD', 'DWORD_BIG_ENDIAN', 'QWORD', 'QWORD_LITTLE_ENDIAN','QWORD', 'QWORD_LITTLE_ENDIAN',
+    'EXPAND_SZ', 'LINK', 'MULTI_SZ', 'NONE', 'RESOURCE_LIST', 'FULL_RESOURCE_DESCRIPTOR', 'RESOURCE_REQUIREMENTS_LIST', 'SZ'
+]
 
-RegistryValueType = Enum('RegistryValueType', {name: value for name, value in winreg.__dict__.items() if name.startswith('REG_')})
+RegistryValueType = Enum('RegistryValueType', {name: value for name, value in winreg.__dict__.items() if name[4:] in VALID_VALUE_TYPE_NAMES})
 
 
 class RegistryValue(NamedTuple):
