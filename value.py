c3fca6070a32b33107ed59f9a6c10fb8dd8f2a8b
thefumbduck <loidiegopes@gmail.com>
2020-04-11 18:59:36 +0200

    Manually write RegistryValueType values

--- value.py
+++ value.py
@@ -1,13 +1,22 @@
 from typing import Any, NamedTuple
-from enum import Enum
+from enum import IntEnum
 import winreg
 
-VALID_VALUE_TYPE_NAMES = [
-    'BINARY', 'DWORD', 'DWORD_BIG_ENDIAN', 'QWORD', 'QWORD_LITTLE_ENDIAN','QWORD', 'QWORD_LITTLE_ENDIAN',
-    'EXPAND_SZ', 'LINK', 'MULTI_SZ', 'NONE', 'RESOURCE_LIST', 'FULL_RESOURCE_DESCRIPTOR', 'RESOURCE_REQUIREMENTS_LIST', 'SZ'
-]
-
-RegistryValueType = Enum('RegistryValueType', {name: value for name, value in winreg.__dict__.items() if name[4:] in VALID_VALUE_TYPE_NAMES})
+# Manually write these to have autocomplete
+class RegistryValueType(IntEnum):
+    BINARY = winreg.REG_BINARY
+    DWORD = winreg.REG_DWORD
+    DWORD_BIG_ENDIAN = winreg.REG_DWORD_BIG_ENDIAN
+    QWORD = winreg.REG_QWORD
+    QWORD_LITTLE_ENDIAN = winreg.REG_QWORD_LITTLE_ENDIAN
+    EXPAND_SZ = winreg.REG_EXPAND_SZ
+    LINK = winreg.REG_LINK
+    MULTI_SZ = winreg.REG_MULTI_SZ
+    NONE = winreg.REG_NONE
+    RESOURCE_LIST = winreg.REG_RESOURCE_LIST
+    FULL_RESOURCE_DESCRIPTOR = winreg.REG_FULL_RESOURCE_DESCRIPTOR
+    RESOURCE_REQUIREMENTS_LIST = winreg.REG_RESOURCE_REQUIREMENTS_LIST
+    SZ = winreg.REG_SZ
 
 
 class RegistryValue(NamedTuple):
