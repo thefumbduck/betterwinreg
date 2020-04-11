0de865ced8cfd340e58d9ffe614a1b953828a1ef
thefumbduck <51268203+thefumbduck@users.noreply.github.com>
2020-04-11 20:31:09 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+from setuptools import setup
+
+HERE = Path(__file__).parent
+README = (HERE / 'README.md').open().read()
+VERSION = '1.0.0'
+
+setup(
+    name='better-winreg',
+    version=VERSION,
+    packages=['betterwinreg'],
+
+    description='A wrapper that makes working with winreg easier',
+    long_description=README,
+    long_description_content_type='text/markdown',
+
+    url='https://github.com/thefumbduck/betterwinreg',
+    project_urls={
+        'Bug tracker': 'https://github.com/thefumbduck/betterwinreg/issues',
+        'Documentation': 'https://github.com/thefumbduck/betterwinreg',
+        'Source code': 'https://github.com/thefumbduck/betterwinreg',
+    },
+
+    author='thefumbduck',
+    author_email='51268203+thefumbduck@users.noreply.github.com',
+    license='GNU GPL v3',
+)
