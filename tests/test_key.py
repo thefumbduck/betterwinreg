982ec536e09f464575cca4bff2d65040c266ff71
thefumbduck <51268203+thefumbduck@users.noreply.github.com>
2020-04-12 12:15:43 +0200

    Add key deletion test

--- tests/test_key.py
+++ tests/test_key.py
@@ -21,16 +21,24 @@ class TestKeyManipulation:
     def test_get(self):
         assert RegistryKey(r'Computer\HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer')['UserSignedIn'].value == 1
 
-    def test_set(self):
+    def test_set_value(self):
         key = RegistryKey(self.SET_TEST_KEY_PATH)
         key['test'] = RegistryValue(42, RegistryValueType.DWORD)
         key.flush()
         assert key['test'].value == 42
 
-    def test_del(self):
+    def test_del_value(self):
         key = RegistryKey(self.SET_TEST_KEY_PATH)
         key['test'] = RegistryValue(42, RegistryValueType.DWORD)
         key.flush()
         del key['test']
         key.flush()
         assert 'test' not in key
+
+    def test_del_key(self):
+        key = RegistryKey(self.SET_TEST_KEY_PATH)
+        if not key.is_key():
+            key.create()
+        key.flush()
+        key.delete()
+        assert not key.is_key()
