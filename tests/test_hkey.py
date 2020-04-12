4564ad9d2e9561923ae3b0126a07a820d039cff5
thefumbduck <51268203+thefumbduck@users.noreply.github.com>
2020-04-12 18:57:28 +0200

    Add abbreviated name test

--- tests/test_hkey.py
+++ tests/test_hkey.py
@@ -6,3 +6,7 @@ from betterwinreg.hkey import Hkey
 def test_id():
     hkey = Hkey('HKEY_CURRENT_USER')
     assert hkey.id_ == winreg.HKEY_CURRENT_USER
+
+
+def test_abbreviated_name():
+    assert Hkey('HKEY_CURRENT_USER') == Hkey('HKCU')
